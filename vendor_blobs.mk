3b446c1045507410687b6c9a0356bd48bf6a7ec3
Hardback-Dev <Hardback.official@gmail.com>
2015-06-05 20:04:12 -0300

    Update vendor_blobs.mk

--- vendor_blobs.mk
+++ vendor_blobs.mk
@@ -52,6 +52,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     vendor/samsung/amazing/proprietary/bin/memsicd:system/bin/memsicd
+    vendor/samsung/amazing/proprietary/lib/libacdapi_azi.so:system/lib/libacdapi_azi.so
+
 # Camera
 PRODUCT_COPY_FILES += \
     vendor/samsung/amazing/proprietary/lib/libcamera.so:system/lib/libcamera.so
